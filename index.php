96863f7cdc99b54c2bb68b8635469b1188fc9b40
Rahmatul Azizah <79456933+Rahmatul647@users.noreply.github.com>
2022-02-16 07:18:49 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+require_once("koneksi.php");
+// Jika sesi dari login belum dibuat maka akan kita kembalikan ke halaman login
+if(!isset($_SESSION['username'])){
+    header("location: login.php");
+}else{
+    // Jika sudah dibuatkan sesi maka akan kita masukkan kedalam variabel
+    $username = $_SESSION['username'];
+}
+
+?>
+
+<html>
+    <head>
+    <style>
+body {
+  background-color: lightblue;
+}
+</style>
+        <title>Aplikasi Pembayaran SPP</title>
+    </head>
+<body>
+  
+<!-- Kita akan panggil menu navigasi -->
+<?php require_once("header.php"); ?>
+<h3>Selamat datang, <?= $username; ?></h3>
+            <br />
+Silahkan dikelola dengan baik yaa :) 
+            <hr />
+<?php require_once("footer.php"); ?>
+
+</body>
+</html>
---
+++ kelas.php
@@ -0,0 +1,74 @@
+<?php
+require_once("require.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <meta charset="UTF-8">
+    <title>CRUD Data Kelas</title>
+</head>
+<body>
+    <!-- Panggil script header -->
+    <?php require_once("header.php"); ?>
+    <!-- Isi Konten -->
+    <h3>Kelas</h3>
+    <p><a href="tambah_kelas.php">Tambah Data</a></p>
+    <table cellspacing="0" border="1" cellpadding="5">
+    <div class="container mt-3">     
+  <table class="table table-dark">
+    <thead>
+        <tr>
+            <td>No. </td>
+            <td>Nama Kelas</td>
+            <td>Kompetensi Keahlian</td>
+            <td>Aksi</td>
+        </tr>
+        </thead>
+    <tbody>
+<?php
+// Kita Konfigurasi Pagging disini
+$totalDataHalaman = 5;
+$data = mysqli_query($db, "SELECT * FROM kelas");
+$hitung = mysqli_num_rows($data);
+$totalHalaman = ceil($hitung / $totalDataHalaman);
+$halAktif = (isset($_GET['hal'])) ? $_GET['hal'] : 1;
+$dataAwal = ($totalDataHalaman * $halAktif) - $totalDataHalaman;
+// Konfigurasi Selesai
+// Kita panggil tabel kelas
+$sql = mysqli_query($db, "SELECT * FROM kelas ORDER BY nama_kelas LIMIT $dataAwal, $totalDataHalaman");
+$no = 1;
+while($r = mysqli_fetch_assoc($sql)){ ?>
+        <tr>
+            <td><?= $no ?></td>
+            <td><?= $r['nama_kelas']; ?></td>
+            <td><?= $r['kompetensi_keahlian']; ?></t0 d>
+            <td><a href="?hapus&id=<?= $r['id_kelas']; ?>">Hapus</a> | 
+                <a href="edit_kelas.php?id=<?= $r['id_kelas']; ?>">Edit</a</td>
+        </tr>
+<?php $no++; } ?>
+    </table>
+<!-- Tampilkan tombol halaman -->
+<?php for($i=1; $i <= $totalHalaman; $i++): ?>
+        <a href="?hal=<?= $i; ?>"><?= $i; ?></a>
+<?php endfor; ?>
+<!-- Selesai -->
+    <hr />
+    <?php require_once("footer.php"); ?>
+</body>
+</html>
+<?php
+// Tombol Hapus ditekan
+if(isset($_GET['hapus'])){
+    $id = $_GET['id'];
+    $hapus = mysqli_query($db, "DELETE FROM kelas WHERE id_kelas='$id'");
+    if($hapus){
+        header("location: kelas.php");
+    }else{
+        echo "<script>alert('Maaf, data tersebut masih terhubung dengan data yang lain');
+        </script>";
+    }
+}
+?>
